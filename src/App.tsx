4386d71708d3e9f424a847b9ec7e37aed5092dbc
doki <doki@dokis-MacBook-Pro-2.local>
2023-03-26 19:12:33 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import styled from '@emotion/styled'
+
+const Box = styled.div`
+  color: hotpink;
+  background-color: yellow;
+`
+
+function App() {
+  return (
+    <div className="App">
+      <Box>Box</Box>
+    </div>
+  )
+}
+
+export default App
