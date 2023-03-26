0f3cbac1a6ffb8701ebc1bfc698291d7f552b611
Duckhee Lee <shldhee@yahoo.co.kr>
2023-03-26 19:18:03 +0900

    src alias 적용 @

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import GlobalStyle from './styles/reset'
+import GlobalStyle from '@/styles/reset'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
